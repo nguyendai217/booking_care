990fe0b58a8aa22769ff87848c183b54dc177cb2
nguyendai217 <nguyendai2171998@gmail.com>
2021-12-07 22:42:22 +0700

    connect to DB postgresq

--- nodejs_server/src/server.js
+++ nodejs_server/src/server.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import intWebRoutes from "./route/web";
 require("dotenv").config();
+import connectDB from "./config/connectDB";
 
 //config server
 let app = express();
@@ -12,8 +13,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // config view engine
 viewEngine(app);
-
 intWebRoutes(app);
+
+connectDB();
 
 let port = process.env.PORT || 8081;
 app.listen(port, () => {
