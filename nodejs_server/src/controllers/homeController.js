75190a94cca02d451f2bc4ec10d28c2844cd80ca
nguyendai217 <nguyendai2171998@gmail.com>
2021-12-11 21:48:47 +0700

    udpate

--- nodejs_server/src/controllers/homeController.js
+++ nodejs_server/src/controllers/homeController.js
@@ -1,9 +1,13 @@
-import db from "../models/index";
 let getHomePage = (req, res) => {
   return res.render("home.ejs");
+};
+
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
 };
 
 // export object
 module.exports = {
   getHomePage: getHomePage,
+  getCRUD: getCRUD,
 };
--- nodejs_server/src/route/web.js
+++ nodejs_server/src/route/web.js
@@ -5,6 +5,7 @@ let router = express.Router();
 
 let intWebRoutes = (app) => {
   router.get("/", homeController.getHomePage);
+  router.get("/crud", homeController.getCRUD);
 
   return app.use("/", router);
 };
