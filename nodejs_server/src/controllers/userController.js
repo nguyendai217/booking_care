ff4d63baba0df81f598a5668928f5cb7064ba870
nguyendai217 <nguyendai2171998@gmail.com>
2021-12-25 21:44:44 +0700

    compare pass

--- nodejs_server/src/controllers/userController.js
+++ nodejs_server/src/controllers/userController.js
@@ -4,12 +4,15 @@ let handleLogin = async (req, res) => {
   let password = req.body.password;
 
   if (!email || !password) {
-    res.status;
+    res.status(500).json({
+      errorCode: 1,
+      message: "Input email or password is empty.",
+    });
   }
 
   let userData = await user.handleLogin(email, password);
   return res.status(200).json({
-    userData,
+    data: userData,
   });
 };
 
--- nodejs_server/src/services/userService.js
+++ nodejs_server/src/services/userService.js
@@ -1,17 +1,36 @@
+import bcrypt from "bcryptjs";
 import db from "../models/index";
 let handleLogin = (email, password) => {
   return new Promise(async (resolve, reject) => {
     try {
       let userData = {};
+
+      // check user exist
       let userExist = await checkUserEmail(email);
       if (userExist) {
-        // compare password
-        resolve("user exist");
+        let user = await db.User.findOne({
+          where: { email: email },
+        });
+        if (user) {
+          // compare password
+          let checkPass = bcrypt.compareSync(password, user.password);
+          if (checkPass) {
+            userData.errCode = 0;
+            userData.errMess = "OK";
+            userData.user = user;
+          } else {
+            userData.errCode = 2;
+            userData.errMess = "Password is not correct !";
+          }
+        } else {
+          userData.errCode = 1;
+          userData.errMess = "User is not found!";
+        }
       } else {
         userData.errCode = 1;
         userData.errMess = "Email is not exist !";
-        resolve(userData);
       }
+      resolve(userData);
     } catch (error) {
       reject(error);
     }
@@ -35,6 +54,26 @@ let checkUserEmail = (userEmail) => {
     }
   });
 };
+
+let comparePassword = (pass) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { email: userEmail },
+      });
+
+      if (user) {
+        let checkPassword = await bcrypt.compareSync(pass, user.password);
+      }
+
+      let password = user.password;
+      bcrypt.compareSync(password, hash);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
 module.exports = {
   handleLogin: handleLogin,
 };
